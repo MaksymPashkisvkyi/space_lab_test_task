101ebcf512f8b3c0b48a4fa640ab8a1f0c9ef852
MaksimPashkovskiy <maximajin@gmail.com>
2022-06-15 12:05:42 +0300

    mode likes feature

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    return redirect(url_for('likes'))
+
+
 @app.route('/likes')
 def likes():
-    error = False
     feedback = None
-    error_message = None
 
     get_request = request.args.get('names')
     split_request = str(get_request).split(",")
     names = list(split_request)
+
+    if get_request is None:
+        return render_template('likes.html', error=True, feedback=None, error_message=None)
 
     for name in names:
         if not name.isalpha():
@@ -21,7 +27,7 @@ def likes():
             error_message = "Имя не может содержать больше 10 символов."
             return render_template('likes.html', error=True, feedback=None, error_message=error_message)
 
-    if not names:
+    if len(names) == 0:
         feedback = 'Это никому не нравится.'
     elif len(names) == 1:
         feedback = f'{names[0]} лайкнул это.'
@@ -32,7 +38,7 @@ def likes():
     elif len(names) > 3:
         feedback = f'{names[0]}, {names[1]} и ещё {len(names) - 2} человека лайкнули это.'
 
-    return render_template('likes.html', error=error, feedback=feedback, error_message=error_message)
+    return render_template('likes.html', error=None, feedback=feedback, error_message=None)
 
 
 if __name__ == "__main__":
